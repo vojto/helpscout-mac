12f4a29ee9adb2f290285edd5faa68ddc6d6f4da
Vojtech Rinik <vojto@rinik.net>
2016-11-05 11:53:13 +0100

    Initial commit

--- HelpScout/AppDelegate.swift
+++ HelpScout/AppDelegate.swift
@@ -11,15 +11,42 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var preferencesWindow: NSWindow!
+    
+    var statusItem: NSStatusItem!
+    var statusMenu: NSMenu!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        let statusBar = NSStatusBar.system()
+        
+        statusItem = statusBar.statusItem(withLength: 40)
+        statusItem.image = NSImage(named: "StatusItem")
+        statusItem.highlightMode = true
+        statusItem.title = "1"
+        
+        statusMenu = NSMenu()
+        statusItem.menu = self.statusMenu
+        
+        let item = NSMenuItem(title: "Updated.", action: nil, keyEquivalent: "")
+        statusMenu.addItem(item)
+        
+        statusMenu.addItem(NSMenuItem.separator())
+        
+        let prefItem = NSMenuItem(title: "Preferences", action: #selector(showPreferences), keyEquivalent: "")
+        statusMenu.addItem(prefItem)
+        
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func showPreferences() {
+        preferencesWindow.makeKeyAndOrderFront(self)
     }
 
 
